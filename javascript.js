85fe62905878df798be7994cb74aeea471fffcdd
OskarSammalisto <43170804+OskarSammalisto@users.noreply.github.com>
2018-11-11 12:35:35 +0100

    All scores now work, just style and cat logo left

--- javascript.js
+++ javascript.js
@@ -10,6 +10,7 @@ var yesRed = document.getElementById('yesRed');
 var noRed = document.getElementById('noRed');
 var starting11 = document.getElementsByClassName('starting11');
 
+
 // scoreboard
 
 var totalScore = document.getElementById('totalScore');
@@ -25,8 +26,9 @@ var goalsOpponentTrue;
 var goalMinuteTrue;
 var yellowsTrue;
 var firstGoalscorerTrue;
-var yesRedTrue;
-var noRedTrue = 0;
+var RedTrue;
+var starting11Guess = [];
+
  //console.log(lineupTrue);
 //functions
 
@@ -41,24 +43,56 @@ function matchResults() {
          goalMinuteTrue = 30;
          yellowsTrue = 2;
          firstGoalscorerTrue = 8;
-         yesRedTrue = 'Yes';
-         noRedTrue = 0;
+         RedTrue = 0;
+
          lineupTrue = [4, 25, 16, 26, 5, 12, 15, 13, 18, 7, 23];
 
     }
     else if(opponent.value == 2 /*everton*/){
+        goalsLiverpoolTrue = 3;
+        goalsOpponentTrue = 2;
+        goalMinuteTrue = 30;
+        yellowsTrue = 2;
+        firstGoalscorerTrue = 8;
+        RedTrue = 0;
+
+        lineupTrue = [4, 25, 16, 26, 5, 12, 15, 13, 18, 7, 23];
 
     }
 
     else if(opponent.value == 3 /*burnley*/){
+        goalsLiverpoolTrue = 3;
+        goalsOpponentTrue = 2;
+        goalMinuteTrue = 30;
+        yellowsTrue = 2;
+        firstGoalscorerTrue = 8;
+        RedTrue = 0;
+
+        lineupTrue = [4, 25, 16, 26, 5, 12, 15, 13, 18, 7, 23];
 
     }
 
     else if(opponent.value == 4 /*leicester*/){
+        goalsLiverpoolTrue = 3;
+        goalsOpponentTrue = 2;
+        goalMinuteTrue = 30;
+        yellowsTrue = 2;
+        firstGoalscorerTrue = 8;
+        RedTrue = 0;
+
+        lineupTrue = [4, 25, 16, 26, 5, 12, 15, 13, 18, 7, 23];
 
     }
 
     else if(opponent.value == 5 /*southampton*/){
+        goalsLiverpoolTrue = 3;
+        goalsOpponentTrue = 0;
+        goalMinuteTrue = 10;
+        yellowsTrue = 2;
+        firstGoalscorerTrue = 29;
+        RedTrue = 0;
+
+        lineupTrue = [4, 25, 14, 26, 5, 12, 27, 13, 18, 7, 23];
 
     }
 }
@@ -77,6 +111,8 @@ function calculate() {
     var p = opponent.value;
     document.getElementById("opponent").options[p].disabled = true;
     document.getElementById('form').reset();
+    console.log(starting11Guess);
+    console.log(lineupTrue);
 }
 
 function totalScoreSum() {
@@ -103,13 +139,7 @@ function goalMinuteFunction() {  //this needs fixing
     if((goalMinute).value == goalMinuteTrue){
         goalMinuteScore.innerHTML = Number(goalMinuteScore.innerHTML) + 20;
     }
-    else if((goalMinuteTrue - 5) >= (goalMinute).value <= (goalMinuteTrue + 5) ){
-        goalMinuteScore.innerHTML = Number(goalMinuteScore.innerHTML) + 10;
-    }
 
-    else if((goalMinuteTrue - 10) >= (goalMinute).value <= (goalMinuteTrue + 10) ){
-        goalMinuteScore.innerHTML = Number(goalMinuteScore.innerHTML) + 2;
-    }
 }
 
 function yellowsFunction() {
@@ -117,9 +147,7 @@ function yellowsFunction() {
     if((yellows).value == yellowsTrue){
         cardsScore.innerHTML = Number(cardsScore.innerHTML) + 10;
     }
-    /*else if((yellowsTrue + 2) >= (yellows).value <= (yellowsTrue + 2)){ //this needs fixing
-        cardsScore.innerHTML = Number(cardsScore.innerHTML) + 5;
-    }*/
+
 }
 
 function firstGoalscorerFunction() {
@@ -131,7 +159,19 @@ function firstGoalscorerFunction() {
 
 function yesRedFunction() { //this needs fixing
     matchResults();
-    if((yesRed).value == yesRedTrue){
+
+    var redGuess;
+
+  if(document.getElementById('yesRed').checked){
+      redGuess = 1;
+  }
+  else{
+      redGuess = 0;
+  }
+
+
+
+    if(redGuess == RedTrue){
         cardsScore.innerHTML = Number(cardsScore.innerHTML) + 10;
     }
     else {
@@ -145,9 +185,28 @@ function lineupFunction() { //this needs fixing
 
     var sum = 0;
 
+
+    starting11Guess[0] = document.getElementById('changeImage1').value;
+    starting11Guess[1] = document.getElementById('changeImage2').value;
+    starting11Guess[2] = document.getElementById('changeImage3').value;
+    starting11Guess[3] = document.getElementById('changeImage4').value;
+    starting11Guess[4] = document.getElementById('changeImage5').value;
+    starting11Guess[5] = document.getElementById('changeImage6').value;
+    starting11Guess[6] = document.getElementById('changeImage7').value;
+    starting11Guess[7] = document.getElementById('changeImage8').value;
+    starting11Guess[8] = document.getElementById('changeImage9').value;
+    starting11Guess[9] = document.getElementById('changeImage10').value;
+    starting11Guess[10] = document.getElementById('changeImage11').value;
+
+
+
+
+
+
+
     for(var i = 0; i < starting11.length ; i++){
         for(var x = 0; x < lineupTrue.length; x++){
-            if(starting11[i] == lineupTrue[x]){
+            if(starting11Guess[i] == lineupTrue[x]){
                 sum++;
             }
         }
